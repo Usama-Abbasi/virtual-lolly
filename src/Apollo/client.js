7973dc370dfe156a0761bdeb341d9974d1448867
usama999abbasi <60782977+usama999abbasi@users.noreply.github.com>
2021-01-09 19:31:17 +0500

    version 1.3

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -5,7 +5,7 @@ var baseUrl;
 process.env.NODE_ENV==="development"?baseUrl="http://localhost:8888":baseUrl="https://usama-bootcamp-virtual-lolly.netlify.app"
 export const client = new ApolloClient({
     link: new HttpLink({
-      uri: `${baseUrl}/.netlify/functions/virtual_lolly`,
+      uri: '/.netlify/functions/virtual_lolly',
       fetch,
     }),
     cache: new InMemoryCache()
