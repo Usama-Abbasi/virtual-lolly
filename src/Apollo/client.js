a25b37ed738f0b307265ce6b6586218a697cb3b0
usama999abbasi <60782977+usama999abbasi@users.noreply.github.com>
2021-01-09 20:03:21 +0500

    updated

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -2,10 +2,10 @@ import fetch from 'cross-fetch';
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 var baseUrl;
-process.env.NODE_ENV==="development"?baseUrl="http://localhost:8888":"https://usama-bootcamp-virtual-lolly.netlify.app"
+process.env.NODE_ENV==="development"?baseUrl="http://localhost:8888":baseUrl="https://usama-bootcamp-virtual-lolly.netlify.app"
 export const client = new ApolloClient({
     link: new HttpLink({
-      uri: `${baseUrl}/.netlify/functions/virtual_lolly`,
+      uri: '/.netlify/functions/virtual_lolly',
       fetch,
     }),
     cache: new InMemoryCache()
